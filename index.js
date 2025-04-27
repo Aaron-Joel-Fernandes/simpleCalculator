393ccf53fb3bb4e3a3a7e756aa74275f74d3f7db
Aaron Joel Fernandes <fenandesaaronjoel@gmail.com>
2025-04-27 13:15:40 +0100

    Adding division Feature

--- index.js
+++ index.js
@@ -19,7 +19,28 @@ function appendOperation(operation) {
 }
 
 function calculate() {
-   
+    if (previousInput === '' || currentInput === '') return;
+            let result;
+            let prev = parseFloat(previousInput);
+            let current = parseFloat(currentInput);
+
+            switch (currentOperation) {
+                
+                case '/':
+                    if (current === 0) {
+                        alert("Cannot divide by zero");
+                        return;
+                    }
+                    result = prev / current;
+                    break;
+                default:
+                    return;
+            }
+
+            currentInput = result.toString();
+            currentOperation = '';
+            previousInput = '';
+            document.getElementById('display').value = currentInput;
     
 }
 
